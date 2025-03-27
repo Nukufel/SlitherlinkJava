f38c4cdd795d428c8b3b7dad2cfab203a96668e9
Niklas Vogel <niklas.vogel@bluewin.ch>
2025-03-27 17:13:24 +0100

    settings

---
+++ src/main/java/ch/Grid.java
@@ -0,0 +1,17 @@
+package ch;
+
+import java.util.ArrayList;
+import java.util.Stack;
+import ch.Action;
+
+
+public class Grid {
+    private Stack<Action> actionStack;
+    private ArrayList<Cell> cells;
+
+    public Grid() {
+
+    }
+
+
+}
---
+++ src/main/java/ch/Settings.java
@@ -0,0 +1,14 @@
+package ch;
+
+import java.util.*;
+
+public class Settings {
+    public static final int gridRows = 5, gridCols = 5;
+    public static final int cellCount = gridRows * gridCols;
+    public static final int failCount = 2000;
+    public static final HashMap<String, HashSet> directions = new HashMap<>();
+
+    public Settings(){
+
+    }
+}
